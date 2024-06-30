17f5ab94ad9041af65d0c5653a6a54a6df87db16
SudiptaAdhikaryJoy <sudiptaadhikaryjoy@gmail.com>
2024-06-30 11:45:56 +0600

    add changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
 
+// Import metadata from the separate file
+import { metadata } from "../../public/metadata"
+export { metadata };
 export default function HomePage() {
   return (
     <div>
