3d7578d88bf106c46c4504f3d6b884cb24724445
SudiptaAdhikaryJoy <sudiptaadhikaryjoy@gmail.com>
2024-07-02 17:33:56 +0600

    button icon changes

--- src/components/ui/features/Todo/TodoContainer.tsx
+++ src/components/ui/features/Todo/TodoContainer.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import TodoCard from "./TodoCard";
+import { Button } from "../../button";
 
 function TodoContainer() {
   return (
     <div>
-      <div className="  ">
+      <div className=" flex justify-between items-center ">
         <button>Add Todo</button>
         <button>Filter</button>
       </div>
-      <div className=" h-[500px] w-full rounded-xl bg-red-500 p-5">
-        <div className=" bg-white rounded-md flex justify-between item-center ">
+      <div className=" h-[500px] w-full rounded-xl bg-gray-300 p-3">
+        <div className=" bg-gray-400 rounded-md flex justify-between item-center p-5">
         <input type="checkbox" name="" id="" />
         <p>Todo Title</p>
         <p>Time</p>
         <p>Description</p>
-        <div>
-            <button>Del</button>
-            <button>Edit</button>
+        <div className=" space-x-3 ">
+            <Button>Del</Button>
+            <Button>Edit</Button>
         </div>
         </div>
       </div>
